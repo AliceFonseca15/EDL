59e40b3f4fce8e6e4f7ae7612bda821ebe828f23
Alice <120612012+AliceFonseca15@users.noreply.github.com>
2025-09-22 18:17:27 +0000

    contrutor

--- Pilha.java
+++ Pilha.java
@@ -1,9 +1,25 @@
+public class PilhaRubroNegra{   
+    private Object[] array;
+    private int t_vermelho;
+    private int t_preto;
+    private int t_capacidade;
 
+    public PilhaRubroNegra(int n){
+        this.t_capacidade = n;
+        this.t_vermelho = -1;
+        this.t_preto = t_capacidade;
+        this.array = new Object[t_capacidade];
 
-public interface Pilha {    
-    public int size();    
-    public boolean isEmpty();
+    }
+    public int size(){
+        return t + 1;
+    }
+    public boolean isEmpty(){
+
+    };
     public Object top() throws PilhaVaziaExcecao;
     public void push(Object o);
     public Object pop() throws PilhaVaziaExcecao;
+
 }
+

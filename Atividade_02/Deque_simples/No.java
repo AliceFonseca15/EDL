fd8f9998ffebe7230813647c4c11721ddf1120c9
Alice <120612012+AliceFonseca15@users.noreply.github.com>
2025-10-13 20:50:11 +0000

    Class No da lista ligada

---
+++ Atividade_02/Deque_simples/No.java
@@ -0,0 +1,18 @@
+public class No{
+    private Object elemento;
+    private No next;
+    public No(Object dado){
+         elemento = dado;
+        next = null;
+    }
+    public Object get_elemento(){
+        return elemento;
+    }
+    public No get_Next(){    
+        return next;
+    }
+    public void set_Next(No n){
+        next = n;      
+    }
+
+}

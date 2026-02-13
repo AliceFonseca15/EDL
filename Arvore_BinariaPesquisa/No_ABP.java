92af7fe6ad388feb640004a3c3fc631be632355c
Alice <120612012+AliceFonseca15@users.noreply.github.com>
2026-02-13 02:18:21 +0000

    remocao de arquivos

--- Arvore_BinariaPesquisa/No_ABP.java
+++ Arvore_BinariaPesquisa/No_ABP.java
@@ -1,37 +1,43 @@
-public class No_ABP{
-    private int elemento;
-    private No_ABP pai;
-    private No_ABP filhoE;
-    private No_ABP filhoD;
-    public No_ABP(){
-        elemento = 0;
+public class No_heap{
+    private Object elemento;
+    private No_heap pai;
+    private No_heap filhoE;
+    private No_heap filhoD;
+    public No_heap(){
+        elemento = null;
         pai = null;
         filhoE = null;
         filhoD = null;
+        chave= int
     }
-    
-    public void set_elemento(int n){
+    public set_chave(int n){
+        chave = n
+    }
+    public get_chave(){
+        return chave
+    }
+    public void set_elemento(Object n){
         elemento = n;
     }    
-    public int get_elemento(){
+    public Object get_elemento(){
         return elemento;
     }
-    public No_ABP get_pai(){    
+    public No_heap get_pai(){    
         return pai;
     }
-    public void set_pai(No_ABP n){
+    public void set_pai(No_heap n){
         pai = n;
     }
-    public No_ABP get_filhoE(){    
+    public No_heap get_filhoE(){    
         return filhoE;
     }
-    public void set_filhoE(No_ABP n){
+    public void set_filhoE(No_heap n){
         filhoE = n;      
     }
-    public No_ABP get_filhoD(){
+    public No_heap get_filhoD(){
         return filhoD;
     }
-    public void set_filhoD(No_ABP n){
+    public void set_filhoD(No_heap n){
         filhoD = n;      
     }
 
